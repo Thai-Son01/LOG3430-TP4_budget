b866949c31ffe7bfe0f922d137f509735a8601ed
hanipeko <gbfspamming1@gmail.com>
2024-03-29 17:17:06 -0400

    a

--- budget/models.py
+++ budget/models.py
@@ -19,8 +19,8 @@ class Project(models.Model):
 
         total_temp = 0
         budget_temp = 100
-        # expense_list_temp = [10, 11, 12, 14, 11]
-        expense_list_temp = [10, 11, 12, 14]
+        expense_list_temp = [10, 11, 12, 14, 11]
+        # expense_list_temp = [10, 11, 12, 14]
         for expenses in expense_list_temp:
             total_temp += expenses
         expense_amount = total_temp
@@ -28,8 +28,8 @@ class Project(models.Model):
 
 
         for expense in expense_list:
-            # total_expense_amount += expense_amount
-            total_expense_amount += expense.amount
+            total_expense_amount += expense_amount
+            # total_expense_amount += expense.amount
 
 
         # temporary solution, because the form currently only allows integer amounts
